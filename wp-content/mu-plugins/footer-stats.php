2876aba9df42d61c71c1d66514f5437d1a494a96
Ryan Hellyer <ryanhellyer@gmail.com>
2014-12-27 02:40:04 +0000

    Automated git commit

--- wp-content/mu-plugins/footer-stats.php
+++ wp-content/mu-plugins/footer-stats.php
@@ -8,5 +8,5 @@ function add_performance_stats() {
 	echo '<!-- Blog ' . get_current_blog_id() . ' was created in ' . timer_stop( 0 ) . ' seconds via ' . get_num_queries() . ' queries -->';
 	echo "\n";
 }
-add_action( 'wp_footer', 'add_performance_stats' );
+add_action( 'wp_footer', 'add_performance_stats', 999 );
 
