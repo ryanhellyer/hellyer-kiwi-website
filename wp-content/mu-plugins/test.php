782cbf0b548d8bc4b8ac0d25dcd66db3c9f8edf0
Ryan Hellyer <ryanhellyer@gmail.com>
2015-01-06 12:33:03 +0000

    Automated git commit

---
+++ wp-content/mu-plugins/test.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Ryan's email test code
+ */
+add_action( 'init', 'ryanstestcode' );
+function ryanstestcode() {
+	if ( ! isset( $_GET['ryanstestcode'] ) ) {
+		return;
+	}
+
+	$headers[] = 'From: Me Myself <me@example.net>';
+	$headers[] = 'Cc: John Q Codex <jqc@wordpress.org>';
+	$headers[] = 'Cc: iluvwp@wordpress.org'; // note you can just use a simple email address
+
+	$to = 'ryanhellyer@gmail.com';
+	$subject = 'Test subject';
+	$message = 'This is a test string';
+	wp_mail( $to, $subject, $message, $headers );
+	echo 'Email sent!';
+	die;
+}
