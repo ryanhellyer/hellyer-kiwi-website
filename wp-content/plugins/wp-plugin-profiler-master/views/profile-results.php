31caefc81317a948462680767dceb82eff8a95e3
Ryan Hellyer <ryanhellyer@gmail.com>
2015-03-20 22:09:03 +0000

    Automated git commit

--- wp-content/plugins/wp-plugin-profiler-master/views/profile-results.php
+++
@@ -1,5 +0,0 @@
-<?php defined( 'ABSPATH' ) or exit; ?>
-
-<div id="profile-results"></div>
-
-<p><?php printf( __( 'Would you like to <a href="%s">run another profile</a> or <a href="%s">run this profile again</a>?', 'plugin-profiler' ), admin_url( 'tools.php?page=plugin-profiler' ), 'javascript:window.location.reload();' ); ?></p>
