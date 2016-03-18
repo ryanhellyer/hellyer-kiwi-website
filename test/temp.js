a30ee9c47faf27ac350aa7956eb82d1d4fc39ac5
Ryan Hellyer <ryanhellyer@gmail.com>
2016-03-18 12:00:08 +0000

    Automated git commit

--- test/temp.js
+++ test/temp.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function($){
 		var height = 0.906666667 * width;
 
 		$('.related-posts article').css( 'height', height+'px' );
-		$('#ryans-featured-image').css( 'height', window.outerHeight+'px' );
-
+		$('#ryans-featured-image').css( 'height', window.innerHeight+'px' );
+alert(window.innerHeight);
 	}
 	else {
 		$('body').removeClass('mobile');		
