ba0ec2bd6f99795245b7b8ff9b68b17a820d325e
Ryan Hellyer <ryanhellyer@gmail.com>
2016-05-13 21:07:05 +0000

    Automated git commit

--- wp-content/plugins/postmatic/core/logging.php
+++ wp-content/plugins/postmatic/core/logging.php
@@ -47,6 +47,12 @@ class Prompt_Logging {
 	 */
 	public static function add_error( $code = '', $message = '', $data = array() ) {
 
+		if ( is_array( $data ) ) {
+			$data['backtrace'] = unserialize( serialize( debug_backtrace() ) );
+		} else if ( $data instanceof WP_Error ) {
+			$data->add_data( unserialize( serialize( debug_backtrace() ) ), 'backtrace' );
+		}
+
 		$wp_error = new WP_Error( $code, $message, $data );
 
 		$log = self::get_log();
