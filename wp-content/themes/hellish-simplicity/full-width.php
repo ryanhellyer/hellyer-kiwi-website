3430c614f3db464f735902eb9c18db428e550763
Ryan Hellyer <ryanhellyer@gmail.com>
2016-06-24 20:58:05 +0000

    Automated git commit

---
+++ wp-content/themes/hellish-simplicity/full-width.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Full width.
+ * Description: A Page Template that displays at full width
+ *
+ * @package Hellish Simplicity
+ * @since Hellish Simplicity 1.4
+ */
+
+require( dirname( __FILE__ ) . '/index.php' );
