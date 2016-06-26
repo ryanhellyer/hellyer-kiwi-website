5f139b574515c772f650c4afa023f1735a6a6242
Ryan Hellyer <ryanhellyer@gmail.com>
2016-06-26 23:03:05 +0000

    Automated git commit

--- wp-content/mu-plugins/test.php
+++ wp-content/mu-plugins/test.php
@@ -2,7 +2,7 @@
 
 //Prompt_Comment_Form_Handling::enqueue_assets();
 
-add_action( 'template_redirect', 'dequeue_postmatic' );
+add_action( 'comment_form', 'dequeue_postmatic', 20 );
 function dequeue_postmatic() {
 	wp_dequeue_style( 'prompt-comment-form' );
 }
