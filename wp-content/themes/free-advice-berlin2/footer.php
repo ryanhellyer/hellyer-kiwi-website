a78558d531690bc53c833d66f4f9197083632fe6
Ryan Hellyer <ryanhellyer@gmail.com>
2016-09-02 15:27:04 +0000

    Automated git commit

--- wp-content/themes/free-advice-berlin2/footer.php
+++ wp-content/themes/free-advice-berlin2/footer.php
@@ -13,7 +13,7 @@
 
 			<span class="alignright">
 				<a href="https://ryan.hellyer.kiwi/contact/">Contact</a>
-				 || 
+				 | 
 				<a href="<?php echo esc_url( home_url( '/legal-notice/' ) ); ?>"><?php _e( 'Legal Notice', 'free-advice-berlin' ) ?></a>
 			</span>
 		</p>
