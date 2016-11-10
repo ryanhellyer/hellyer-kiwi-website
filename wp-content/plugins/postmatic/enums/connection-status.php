aa87fd98a1052aca6726acb80a3569f8dc58377e
Ryan Hellyer <ryanhellyer@gmail.com>
2016-11-10 20:43:04 +0000

    Automated git commit

--- wp-content/plugins/postmatic/enums/connection-status.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Prompt_Enum_Connection_Status {
-	const CREATED = 'created';
-	const CONNECTED = 'connected';
-	const UNREACHABLE = 'unreachable';
-	const ISOLATED = 'isolated';
-}
--- wp-content/plugins/postmatic/templates/email-options-tab-footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var string $upgrade_url
- * @var string $image_url
- */
-?>
-<div class="tab-footer">
-
-</div>
