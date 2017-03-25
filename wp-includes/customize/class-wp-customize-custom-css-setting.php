11daf552a26d3f7b317e0d2815b2ac8f696970bb
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-03-25 02:01:01 +0000

    Automated git commit

--- wp-includes/customize/class-wp-customize-custom-css-setting.php
+++ wp-includes/customize/class-wp-customize-custom-css-setting.php
@@ -191,7 +191,13 @@ final class WP_Customize_Custom_CSS_Setting extends WP_Customize_Setting {
 			$imbalanced = true;
 		}
 
-		// Ensure double quotes are equal.
+		// Ensure single quotes are equal.
+		if ( ! $this->validate_equal_characters( '\'', $css ) ) {
+			$validity->add( 'unequal_single_quotes', __( 'Your single quotes <code>\'</code> are uneven. Make sure there is a closing <code>\'</code> for every opening <code>\'</code>.' ) );
+			$imbalanced = true;
+		}
+
+		// Ensure single quotes are equal.
 		if ( ! $this->validate_equal_characters( '"', $css ) ) {
 			$validity->add( 'unequal_double_quotes', __( 'Your double quotes <code>"</code> are uneven. Make sure there is a closing <code>"</code> for every opening <code>"</code>.' ) );
 			$imbalanced = true;
