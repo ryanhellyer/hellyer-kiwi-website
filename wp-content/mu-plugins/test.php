320869caa2bc7081598207ddc1770026783a9fa8
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-09-17 20:43:01 +0000

    Automated git commit

--- wp-content/mu-plugins/test.php
+++ wp-content/mu-plugins/test.php
@@ -9,3 +9,25 @@ function dequeue_postmatic() {
 	wp_dequeue_script( 'prompt-comment-form' );
 
 }
+
+
+
+add_action( 'init', 'bla' );
+function bla() {
+
+	if ( ! isset( $_GET['testmail'] ) ) {
+		return;
+	}
+
+	$display_name = 'Ryan Hellyer';
+	wp_mail(
+		'ryanhellyer@gmail.com',
+		'New Undiecar driver: ' . $display_name,
+		'<a href="' . esc_url( 'https://undiecar.com/member/' . sanitize_title( $display_name ) ) . '/">' . esc_html( $display_name ) . '</a> has signed up to the Undiecar Championship.'
+	);
+
+	echo 'Mail sent';
+	die;
+}
+
+
