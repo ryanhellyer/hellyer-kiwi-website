80a96736a07a0c8f9663b8da9dd0424ed4507759
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-10-21 14:31:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/inc/class-src-core.php
+++ wp-content/themes/undiecar/inc/class-src-core.php
@@ -75,6 +75,7 @@ class SRC_Core {
 		 *  Otherwise recalculate the results (normal mid-season)
 		 */
 		$stored_results = get_post_meta( $season_id, '_stored_results', true );
+$content .= '<!--' . print_r( $stored_results, true ) . '-->';
 		$use_stored_results = get_post_meta( $season_id, '_permanently_store_results', true );
 		if ( '' === $stored_results || false === $use_stored_results ) {
 
@@ -215,7 +216,7 @@ class SRC_Core {
 
 		} // End of championship positions calculation
 		else {
-			$content .= "\n\n<!-- Using permanently stored results -->\n";
+			$content .= "<!-- Using permanently stored results -->";
 		}
 
 		if ( false === $title ) {
