d88f45c5735fcdbd573f787b8d8a91ba8667f275
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-10-31 17:01:03 +0000

    Automated git commit

--- wp-includes/js/twemoji.js
+++ wp-includes/js/twemoji.js
@@ -88,7 +88,7 @@ var twemoji = (function (
        */
       onerror: function onerror() {
         if (this.parentNode) {
-          this.parentNode.replaceChild(createText(this.alt, false), this);
+          this.parentNode.replaceChild(createText(this.alt), this);
         }
       },
 
@@ -159,13 +159,13 @@ var twemoji = (function (
        * @example
        *
        *  twemoji.parse("I \u2764\uFE0F emoji!");
-       *  // I <img class="emoji" draggable="false" alt="❤️" src="/assets/2764.gif"/> emoji!
+       *  // I <img class="emoji" draggable="false" alt="❤️" src="/assets/2764.gif"> emoji!
        *
        *
        *  twemoji.parse("I \u2764\uFE0F emoji!", function(iconId, options) {
        *    return '/assets/' + iconId + '.gif';
        *  });
-       *  // I <img class="emoji" draggable="false" alt="❤️" src="/assets/2764.gif"/> emoji!
+       *  // I <img class="emoji" draggable="false" alt="❤️" src="/assets/2764.gif"> emoji!
        *
        *
        * twemoji.parse("I \u2764\uFE0F emoji!", {
@@ -174,7 +174,7 @@ var twemoji = (function (
        *     return '/assets/' + options.size + '/' + iconId + options.ext;
        *   }
        * });
-       *  // I <img class="emoji" draggable="false" alt="❤️" src="/assets/72x72/2764.png"/> emoji!
+       *  // I <img class="emoji" draggable="false" alt="❤️" src="/assets/72x72/2764.png"> emoji!
        *
        */
       parse: parse,
@@ -237,8 +237,8 @@ var twemoji = (function (
     // used to find HTML special chars in attributes
     rescaper = /[&<>'"]/g,
 
-    // nodes with type 1 which should **not** be parsed
-    shouldntBeParsed = /^(?:iframe|noframes|noscript|script|select|style|textarea)$/,
+    // nodes with type 1 which should **not** be parsed (including lower case svg)
+    shouldntBeParsed = /IFRAME|NOFRAMES|NOSCRIPT|SCRIPT|SELECT|STYLE|TEXTAREA|[a-z]/,
 
     // just a private shortcut
     fromCharCode = String.fromCharCode;
@@ -256,8 +256,8 @@ var twemoji = (function (
    * @param   string  text used to create DOM text node
    * @return  Node  a DOM node with that text
    */
-  function createText(text, clean) {
-    return document.createTextNode(clean ? text.replace(UFE0Fg, '') : text);
+  function createText(text) {
+    return document.createTextNode(text);
   }
 
   /**
@@ -301,10 +301,9 @@ var twemoji = (function (
         // collect them to process emoji later
         allText.push(subnode);
       }
-      // ignore all nodes that are not type 1, that are svg, or that
+      // ignore all nodes that are not type 1 or that
       // should not be parsed as script, style, and others
-      else if (nodeType === 1 && !('ownerSVGElement' in subnode) &&
-          !shouldntBeParsed.test(subnode.nodeName.toLowerCase())) {
+      else if (nodeType === 1 && !shouldntBeParsed.test(subnode.nodeName)) {
         grabAllTextNodes(subnode, allText);
       }
     }
@@ -366,7 +365,7 @@ var twemoji = (function (
         index = match.index;
         if (index !== i) {
           fragment.appendChild(
-            createText(text.slice(i, index), true)
+            createText(text.slice(i, index))
           );
         }
         rawText = match[0];
@@ -394,7 +393,7 @@ var twemoji = (function (
           modified = true;
           fragment.appendChild(img);
         }
-        if (!img) fragment.appendChild(createText(rawText, false));
+        if (!img) fragment.appendChild(createText(rawText));
         img = null;
       }
       // is there actually anything to replace in here ?
@@ -402,7 +401,7 @@ var twemoji = (function (
         // any text left to be added ?
         if (i < text.length) {
           fragment.appendChild(
-            createText(text.slice(i), true)
+            createText(text.slice(i))
           );
         }
         // replace the text node only, leave intact
@@ -460,7 +459,7 @@ var twemoji = (function (
             ret = ret.concat(' ', attrname, '="', escapeHTML(attrib[attrname]), '"');
           }
         }
-        ret = ret.concat('/>');
+        ret = ret.concat('>');
       }
       return ret;
     });
