901fbe9df1749a3209d0feb1bea1b91d9364eff2
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-12-02 18:36:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/inc/class-src-teams.php
+++ wp-content/themes/undiecar/inc/class-src-teams.php
@@ -24,10 +24,8 @@ class SRC_Teams extends SRC_Core {
 		add_action( 'save_post',       array( $this, 'drivers_save' ), 10, 2 );
 
 		// Add filters
-		if ( isset( $_GET['test'] ) ) {
-			add_filter( 'the_content',     array( $this, 'drivers_list' ) );
-			add_filter( 'the_content',     array( $this, 'add_gallery' ) );
-		}
+		add_filter( 'the_content',     array( $this, 'drivers_list' ) );
+		add_filter( 'the_content',     array( $this, 'add_gallery' ) );
 
 	}
 
