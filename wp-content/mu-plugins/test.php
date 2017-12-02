ea869f428d237248dacd43c9cf6b0054aafcf41f
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-12-02 00:26:01 +0000

    Automated git commit

--- wp-content/mu-plugins/test.php
+++ wp-content/mu-plugins/test.php
@@ -5,4 +5,4 @@ function do_not_cache_feeds(&$feed) {
    $feed->enable_cache(false);
  }
 
- add_action( 'wp_feed_options', 'do_not_cache_feeds' );
+// add_action( 'wp_feed_options', 'do_not_cache_feeds' );
