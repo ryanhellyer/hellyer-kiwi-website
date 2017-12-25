4840ffd9de55f0b2db3c548e78a3e137ab5a2f53
Ryan Hellyer <ryan@pressabl3.localdomain>
2017-12-25 17:33:01 +0000

    Automated git commit

---
+++ wp-content/plugins/undiecar-new-media-rss-feed/feed-template.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Template Name: New Media RSS Feed
+ */
+
+header( 'Content-Type: ' . feed_content_type( 'rss-http' ) . '; charset=' . get_option( 'blog_charset' ), true );
+
+?>
+<?xml version="1.0" encoding="<?php echo esc_attr( get_option( 'blog_charset' ) ); ?>"?>
+<rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	<?php do_action( 'rss2_ns' ); ?>>
+<channel>
+	<title><?php bloginfo_rss( 'name' ); ?> - Feed</title>
+	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
+	<link><?php bloginfo_rss( 'url' ) ?></link>
+	<description><?php bloginfo_rss( 'description' ) ?></description>
+	<lastBuildDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_lastpostmodified( 'GMT' ), false ); ?></lastBuildDate>
+	<language><?php echo get_option( 'rss_language' ); ?></language>
+	<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
+	<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+	<?php do_action( 'rss2_head' ); ?>
+	<?php
+
+	// Loop through each team
+	$media_query = new WP_Query( array(
+		'post_type'      => 'attachment',
+		'post_status'    => 'any',
+		'posts_per_page' => 100,
+		'no_found_rows'  => true,
+		'update_post_meta_cache' => false,
+		'update_post_term_cache' => false,
+	) );
+	if ( $media_query->have_posts() ) {
+		while ( $media_query->have_posts() ) {
+			$media_query->the_post();
+
+			$parent_id = wp_get_post_parent_id( get_the_ID() );
+			if ( 'event' === get_post_type( $parent_id ) ) {
+				?>
+		<item>
+			<title><?php the_title(); ?></title>
+			<link><?php the_permalink(); ?></link>
+			<pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_the_date( 'U' ), false ); ?></pubDate>
+			<description><![CDATA[<?php the_excerpt(); ?>]]></description>
+			<content:encoded><![CDATA[<?php the_content(); ?>]]></content:encoded>
+			<?php do_action( 'rss2_item' ); ?>
+		</item><?php
+			}
+
+		}
+	}
+
+?>
+</channel>
+</rss>
