1cc8dac715af84cd515c527f9d19394cfbe2343b
Ryan Hellyer <ryan@pressabl3.localdomain>
2018-02-13 04:21:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/inc/class-src-register.php
+++ wp-content/themes/undiecar/inc/class-src-register.php
@@ -118,6 +118,10 @@ class SRC_Register extends SRC_Core {
 			if ( ! defined( 'SRC_USERNAME_EXISTS' ) || defined( 'SRC_EMAIL_EXISTS' ) ) {
 				$user = get_user_by( 'email', $email );
 				$message_text = __( 'You already have an account ;) You can login with it if you like.', 'src' );
+
+$message_text = 'The form below is broken, please use the login page until then ... https://undiecar.com/login/';
+
+
 				$display_name = $user->display_name;
 				define( 'SRC_USERNAME_EXISTS', true );
 			}
