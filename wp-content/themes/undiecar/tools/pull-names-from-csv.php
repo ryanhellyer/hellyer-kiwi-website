76df930dd185555985a57943d659925822b4e7eb
Ryan Hellyer <ryanhellyer@gmail.com>
2018-02-25 18:10:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/tools/pull-names-from-csv.php
+++ wp-content/themes/undiecar/tools/pull-names-from-csv.php
@@ -80,7 +80,7 @@ foreach ( array_merge(
 
 
 $events = array(
-	'indy-fixed-s1-w1-w9' => array( // might be worth inviting B-license holders too
+	'fixed-indy-iowa-s1' => array( // might be worth inviting B-license holders too
 		'incident_ratio_1' => 1,
 		'incident_ratio_2' => 1,
 		'incident_ratio_3' => 1,
@@ -89,6 +89,14 @@ $events = array(
 		'time_3'           => 999,
 	),
 	/*
+	'indy-fixed-s1-w1-w9' => array( // might be worth inviting B-license holders too
+		'incident_ratio_1' => 1,
+		'incident_ratio_2' => 1,
+		'incident_ratio_3' => 1,
+		'time_1'           => 999,
+		'time_2'           => 999,
+		'time_3'           => 999,
+	),
 	'dallara-dash-9' => array(
 		'incident_ratio_1' => 1,
 		'incident_ratio_2' => 1,
