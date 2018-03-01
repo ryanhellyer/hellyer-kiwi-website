216bb66e962041e681cf25eeb643d6995dfba14b
Ryan Hellyer <ryanhellyer@gmail.com>
2018-03-01 20:25:01 +0000

    Automated git commit

---
+++ wp-content/plugins/pushpress-coaches/includes/class-pushpress-coaches-deactivator.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @since      1.0.0
+ *
+ * @package    pushpress_coaches
+ * @subpackage pushpress_coaches/includes
+ */
+
+class Pushpress_Coaches_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
