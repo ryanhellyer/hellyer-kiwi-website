0ebb232ba736ee7f1d89c6b8723e0e96bba4d948
Ryan Hellyer <ryanhellyer@gmail.com>
2018-05-21 13:23:02 +0000

    Automated git commit

---
+++ wp-content/themes/cuckoo/index.php
@@ -0,0 +1,15 @@
+<style>
+body {
+	margin: 0;
+	padding: 0;
+	background: #fff;
+}
+h1 {
+	font-family: sans-serif;
+	font-size: 80px;
+	color: #222;
+	text-align: center;
+	margin-top: 200px;
+}
+</style>
+<h1>Coming Soon!</h1>
