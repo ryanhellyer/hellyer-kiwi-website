d43bbb18b72a298aca844eb6c7d21a456d0c9e64
Ryan Hellyer <ryanhellyer@gmail.com>
2018-07-16 15:53:02 +0000

    Automated git commit

--- strattic/deploy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$command = '/tmp/lock1 /usr/local/bin/spider.sh ' . STRATTIC_CLOUDFRONT_DOMAIN . ' ' . STRATTIC_CLOUDFRONT_ID . ' ' . STRATTIC_STAGE_DOMAIN . ' ' . STRATTIC_S3_BUCKET . ' ' . STRATTIC_EMAIL . ' ' . STRATTIC_PASSWORD;
-
-$output = shell_exec( 'flock -n 4 ' . $command );
-echo $command;
-
-die;
--- strattic/strattic-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define( 'STRATTIC_CLOUDFRONT_DOMAIN', '$cloudfront_domain' );
-define( 'STRATTIC_CLOUDFRONT_ID', '$cloudfront_id' );
-define( 'STRATTIC_STAGE_DOMAIN', '$stage_domain' );
-define( 'STRATTIC_S3_BUCKET', '$s3_bucket' );
-define( 'STRATTIC_EMAIL', 'support@strattic.com' );
-define( 'STRATTIC_PASSWORD', '$password' );
