fe5a69b1740f54c92de12b638ddddb42a5dc8a3c
Ryan Hellyer <ryanhellyer@gmail.com>
2018-08-04 22:45:01 +0000

    Automated git commit

--- wp-content/plugins/strattic-search/assets/search.js
+++ wp-content/plugins/strattic-search/assets/search.js
@@ -8,34 +8,36 @@
 		function () {
 
 			var results_html = document.getElementById( 'strattic-search-results' );
-results_html.innerHTML = '<span id="strattic-search-loading">LOADING SEARCH RESULTS</span>';
-
 			var fuse = new Fuse(data, strattic_search_settings);
 			var results = fuse.search( get_search_string() );
-			if (Object.keys(results).length > 0){
+
+			if ( Object.keys( results ).length > 0 ) {
+
+				var intro_template = document.getElementById( 'tmpl-strattic-search-intro-template' ).innerHTML;
+				var args = new Object();
+				args.search_string = get_search_text();
+				results_html.innerHTML += Mustache.render( intro_template, args );
 
 				// Process results
-				results.sort(function(a,b){return new Date(b.rdate) - new Date(a.rdate) });
+				results.sort( function( a, b ) { return new Date( b.rdate ) - new Date( a.rdate ) } );
 				var i = -1;
-				for (i in results) {
-					if (results.hasOwnProperty(i)) {
-						var result = results[i];
+				for ( i in results ) {
+					if ( results.hasOwnProperty( i ) ) {
+						var result = results[ i ];
 						result.number = ( parseInt( i ) + 1 );
 						result.url = strattic_home_url + result.path;
-						var template = document.getElementById( 'tmpl-strattic-search-results-template' ).innerHTML;
-						results_html.innerHTML += Mustache.render( template, result );
+						var results_template = document.getElementById( 'tmpl-strattic-search-results-template' ).innerHTML;
+						results_html.innerHTML += Mustache.render( results_template, result );
 					}
 				}
-
-//var results_html = document.getElementById( 'strattic-search-loading' );
 
 			} else {
 
 				// Show no results
 				var result = new Object();
 				result.search_string = get_search_text();
-				var template = document.getElementById( 'tmpl-strattic-search-not-found-template' ).innerHTML;
-				results_html.innerHTML = Mustache.render( template, result );
+				var results_not_found_template = document.getElementById( 'tmpl-strattic-search-not-found-template' ).innerHTML;
+				results_html.innerHTML = Mustache.render( results_not_found_template, result );
 
 			}
 		}
@@ -45,12 +47,12 @@ results_html.innerHTML = '<span id="strattic-search-loading">LOADING SEARCH RESU
 	 * Get the raw search string via the URL.
 	 */
 	function get_search_string() {
-		var query = window.location.search.substring(1);
-		var vars = query.split("&");
-		for (var i=0;i<vars.length;i++) {
-			var pair = vars[i].split("=");
-			if (pair[0] == 's') {
-				return pair[1];
+		var query = window.location.search.substring( 1 );
+		var vars = query.split( '&' );
+		for ( var i = 0; i < vars.length; i++ ) {
+			var pair = vars[i].split( '=' );
+			if ( pair[ 0 ] == 's' ) {
+				return pair[ 1 ];
 			}
 		}
 	}
@@ -59,7 +61,7 @@ results_html.innerHTML = '<span id="strattic-search-loading">LOADING SEARCH RESU
 	 * Get the search string in text form, ready for output to the page.
 	 */
 	function get_search_text() {
-		 return decodeURIComponent(( get_search_string() +'').replace(/\+/g, '%20'));
+		 return decodeURIComponent( ( get_search_string() +'' ).replace( /\+/g, '%20' ) );
 	}
 
 })();
