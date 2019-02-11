b97915d8101bc6ac4b1bdce5f5040108ba47b269
Ryan Hellyer <ryanhellyer@gmail.com>
2019-02-11 21:28:01 +0000

    Automated git commit

--- wp-content/plugins/undiecar-discord/undiecar-discord.php
+++ wp-content/plugins/undiecar-discord/undiecar-discord.php
@@ -66,7 +66,7 @@ if ( isset( $_GET['test_discord'] ) ) {add_action( 'admin_init', array( $this, '
 				if ( 'video' === $embed[ 'type' ] ) {
 
 					$video_url     = esc_url(  $embed[ 'url' ] );
-echo $video_url . '<br>'."\n";die;
+echo $video_url . "\n";die;
 					$post_title = '';
 					if ( isset( $embed[ 'title' ] ) ) {
 						$post_title    = esc_html( 'Video:', 'undiecar' ) . ' ' . esc_html( $embed[ 'title' ] );
@@ -137,7 +137,7 @@ echo $video_url . '<br>'."\n";die;
 
 			}
 
-
+die;
 			// Import the attached images
 			foreach ( $message['attachments'] as $key2 => $attachment ) {
 				$message_id = $message['id'];
