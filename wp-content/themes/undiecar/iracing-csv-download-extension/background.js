afe74c79b904bc96ddcfa815ad6f66c4e87e3abf
Ryan Hellyer <ryanhellyer@gmail.com>
2019-04-22 11:40:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/iracing-csv-download-extension/background.js
+++ wp-content/themes/undiecar/iracing-csv-download-extension/background.js
@@ -1,3 +1,4 @@
+
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 	if (message.text == "download csvs") {
 
@@ -5,11 +6,8 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 			subsessions: '',
 		}, function(items) {
 
-		//alert('pooper');
 
-//			if (
-//				window.location.href === 'http://members.iracing.com/membersite/member/SeriesRaceResults.do?season=2052'
-//			) {
+			setTimeout(function() {
 
 				// If empty members list, then bail out
 				items.subsessions.replace(/\s+/, "") 
@@ -21,18 +19,22 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 				for ( var i = 0; i < subsessions_array.length; i++ ) {
 					var url = 'http://members.iracing.com/membersite/member/GetEventResultsAsCSV?subsessionid=' + subsessions_array[i] + '&simsesnum=0&includeSummary=1';
 
-//					var url = 'http://members.iracing.com/membersite/member/GetEventResultsAsCSV?subsessionid=23201224&simsesnum=0&includeSummary=1';
-					chrome.downloads.download({
-						url: url // The object URL can be used as download URL
-					});
+//console.log('xxx');
 
-		//			alert( url );
-		console.log( subsessions_array[i] );
-		//		    window.open( url, '_self' );
+//					var includes = url.includes("csv");
+//					if ( true === includes ) {
+
+						chrome.downloads.download({
+							url: url // The object URL can be used as download URL
+						});
+
+						console.log( subsessions_array[i] );
+//					}
 
 				}
 
-//			}
+			}, 1000 );
+
 
 		});
 
--- wp-content/themes/undiecar/iracing-csv-download-extension/iracing-csv-download.js
+++ wp-content/themes/undiecar/iracing-csv-download-extension/iracing-csv-download.js
@@ -1,5 +1,36 @@
+// Add session IDs to console log
+setInterval(collect_the_ids, 5000);
+function collect_the_ids() {
+	var collected_ids = '';
+
+
+	function ShowResults(value, index, ar) {
+		var href = value.href;
+
+		var includes = href.includes("javascript:launchEventResult(");
+		if ( true === includes ) {
+			href = href.replace("javascript:launchEventResult(", "");
+			href = href.replace(")", "");
+
+			var bits = href.split(",");
+			var session_id = bits[0];
+			collected_ids = collected_ids + ',' + session_id;
+
+		}
+
+	}
+
+	var link_tags = document.getElementsByTagName('A');
+	link_tags = Array.prototype.slice.call(link_tags)
+	link_tags.forEach(ShowResults);
+
+	console.log( collected_ids );
+}
+
+
+
 if (
-	window.location.href === 'http://members.iracing.com/membersite/member/SeriesRaceResults.do?season=2052'
+	window.location.href === 'https://members.iracing.com/membersite/member/SeriesRaceResults.do?season=2052'
 ) {
 	console.log('sending message');
 	chrome.runtime.sendMessage({text: "download csvs"});
