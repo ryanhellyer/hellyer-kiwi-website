180d3a7c11db1c55df0e5ff0b4ebf12036647fa3
Ryan Hellyer <ryanhellyer@gmail.com>
2019-05-01 17:21:02 +0000

    Automated git commit

--- wp-content/themes/undiecar/tools/pull-names-from-csv.php
+++ wp-content/themes/undiecar/tools/pull-names-from-csv.php
@@ -70,7 +70,7 @@ foreach ( array_merge(
 
 
 $events = array(
-	'2019-s2-c-fixed-trucks' => array(
+	'2019-s3-radicals' => array(
 		'incident_ratio_1' => 1,
 		'incident_ratio_2' => 1,
 		'incident_ratio_3' => 1,
@@ -79,6 +79,14 @@ $events = array(
 		'time_3'           => 999,
 	),
 /*
+	'2019-s2-c-fixed-trucks' => array(
+		'incident_ratio_1' => 1,
+		'incident_ratio_2' => 1,
+		'incident_ratio_3' => 1,
+		'time_1'           => 999,
+		'time_2'           => 999,
+		'time_3'           => 999,
+	),
 	'roval-s1-2019' => array(
 		'incident_ratio_1' => 1,
 		'incident_ratio_2' => 1,
