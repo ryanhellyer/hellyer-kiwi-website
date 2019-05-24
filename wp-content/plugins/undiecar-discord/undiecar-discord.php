e9b4300144d93505b9560c7325cc1b1cd9b41534
Ryan Hellyer <ryanhellyer@gmail.com>
2019-05-24 22:00:02 +0000

    Automated git commit

--- wp-content/plugins/undiecar-discord/undiecar-discord.php
+++ wp-content/plugins/undiecar-discord/undiecar-discord.php
@@ -54,7 +54,7 @@ if ( isset( $_GET['test_discord'] ) ) {add_action( 'admin_init', array( $this, '
 		$last_message_id = $channel['last_message_id'];
 
 		$messages = $discord->channel->getChannelMessages(['channel.id' => DISCORD_PHOTO_CHANNEL_ID,'before'=> 0,'after'=> 0,'around'=> (int) $last_message_id,'limit'=> 100]);
-print_r( $messages );die;
+
 		$dir = wp_upload_dir();
 		$base_dir = $dir['basedir'];
 
@@ -98,15 +98,17 @@ print_r( $messages );die;
 					}
 
 					$content = wp_kses_post(
-						'<p>' .
-						sprintf(
-							esc_html__( 'The following video was kindly created by  %s.', 'undiecar' ),
-							'<a href="' . esc_url( $channel ) . '">' . esc_html( $author ) . '</a>'
+						wpautop( $description ) .
+						wpautop(
+							sprintf(
+								esc_html__( 'The following video was kindly created by  %s.', 'undiecar' ),
+								'<a href="' . esc_url( $channel ) . '">' . esc_html( $author ) . '</a>'
+							)
 						) .
 						'</p>' . 
 						"\n\n" .$video_url . "\n\n"
 					);
-
+print_r( $content );die;
 					// Create video post - if it doesn't already exist
 					$post_slug = sanitize_title( $post_title );
 					$existing_post = get_page_by_path( $post_slug, OBJECT, 'video' );
