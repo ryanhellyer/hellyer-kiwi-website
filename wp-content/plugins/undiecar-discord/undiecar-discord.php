7c9f52cb8f21a18a66d4b61ea448dfc56fec7fbe
Ryan Hellyer <ryanhellyer@gmail.com>
2019-05-24 22:02:01 +0000

    Automated git commit

--- wp-content/plugins/undiecar-discord/undiecar-discord.php
+++ wp-content/plugins/undiecar-discord/undiecar-discord.php
@@ -105,7 +105,7 @@ if ( isset( $_GET['test_discord'] ) ) {add_action( 'admin_init', array( $this, '
 								'<a href="' . esc_url( $channel ) . '">' . esc_html( $author ) . '</a>'
 							)
 						) .
-						"\n\n" . '<a href="' . esc_url( $video_url ) . '">' . esc_url( $video_url ) . '</a>' . esc_url( $video_url ) . "\n\n"
+						"\n\n" . '<a href="' . esc_url( $video_url ) . '">' . esc_url( $video_url ) . '</a>' . "\n\n" . esc_url( $video_url ) . "\n\n"
 					);
 print_r( $content );die;
 					// Create video post - if it doesn't already exist
