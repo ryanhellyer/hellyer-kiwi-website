72dd913b591663856052ea46a633bc0dbc0f4a7f
Ryan Hellyer <ryanhellyer@gmail.com>
2019-07-12 22:01:02 +0000

    Automated git commit

--- wp-content/plugins/gutenberg/gutenberg.php
+++ wp-content/plugins/gutenberg/gutenberg.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gutenberg
  * Plugin URI: https://github.com/WordPress/gutenberg
  * Description: Printing since 1440. This is the development plugin for the new block editor in core.
- * Version: 6.1.0
+ * Version: 6.1.1
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -11,8 +11,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '6.1.0' );
-define( 'GUTENBERG_GIT_COMMIT', 'cf066a936ea496cfd6805bff650b3f79e6ea7191' );
+define( 'GUTENBERG_VERSION', '6.1.1' );
+define( 'GUTENBERG_GIT_COMMIT', '3b51c794d5d2b41c9ba52b8d276e4b951001ca7f' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
