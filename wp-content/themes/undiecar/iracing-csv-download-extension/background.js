fe584d956be214309eaa055cae888dd3307752cc
Ryan Hellyer <ryanhellyer@gmail.com>
2019-08-14 23:46:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/iracing-csv-download-extension/background.js
+++ wp-content/themes/undiecar/iracing-csv-download-extension/background.js
@@ -7,50 +7,48 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 		}, function(items) {
 
 
-			setTimeout(function() {
+			// If empty members list, then bail out
+			items.subsessions.replace(/\s+/, "") 
+			if ( '' === items.subsessions ) {
+				return items;
+			}
 
-				// If empty members list, then bail out
-				items.subsessions.replace(/\s+/, "") 
-				if ( '' === items.subsessions ) {
-					return items;
-				}
+			var subsessions_array = items.subsessions.split(",");
+			for ( var i = 0; i < subsessions_array.length; i++ ) {
+				var url = 'http://members.iracing.com/membersite/member/GetEventResultsAsCSV?subsessionid=' + subsessions_array[i] + '&simsesnum=0&includeSummary=1';
 
-				var subsessions_array = items.subsessions.split(",");
-				for ( var i = 0; i < subsessions_array.length; i++ ) {
-					var url = 'http://members.iracing.com/membersite/member/GetEventResultsAsCSV?subsessionid=' + subsessions_array[i] + '&simsesnum=0&includeSummary=1';
+				setTimeout(
+					function(url) {
 
-//console.log('xxx');
-
-//					var includes = url.includes("csv");
-//					if ( true === includes ) {
+//alert('url: ' + url);
 
 						chrome.downloads.download({
 							url: url // The object URL can be used as download URL
 						});
 
-						console.log( subsessions_array[i] );
-//					}
-
-				}
-
-			}, 1000 );
-
-
-		});
-
-	}
-});
-
+					},
+					( 900 * i ),
+					url
+				);
 /*
-chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-	if (message.text == "download csvs") {
-		console.log('message received');
 
-		var url = 'http://members.iracing.com/membersite/member/GetEventResultsAsCSV?subsessionid=23201224&simsesnum=0&includeSummary=1';
-		chrome.downloads.download({
-			url: url // The object URL can be used as download URL
+*/
+//						console.log( subsessions_array[i] );
+
+			}
+
 		});
 
 	}
 });
-*/
+
+
+function undiecar_get_csv( url ) {
+
+	console.log('URL: ' + url);
+
+//	chrome.downloads.download({
+//		url: url // The object URL can be used as download URL
+//	});
+
+}
