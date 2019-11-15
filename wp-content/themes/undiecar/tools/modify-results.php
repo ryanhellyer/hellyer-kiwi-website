df401abb3aeb0afb853a01f8e30307a6dff1943e
Ryan Hellyer <ryanhellyer@gmail.com>
2019-11-15 12:12:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/tools/modify-results.php
+++ wp-content/themes/undiecar/tools/modify-results.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if ( ! isset( $_GET['modify_result'] ) ) {
 	return;
 }
@@ -14,10 +13,7 @@ $new_meta[0] = $old_meta[1];
 $new_meta[1] = $old_meta[2];
 $new_meta[2] = $old_meta[3];
 $new_meta[3] = $old_meta[4];
-$new_meta[4] = $old_meta[0];
+$new_meta[4] = $old_meta[5];
+$new_meta[5] = $old_meta[0];
 
-//update_meta( $post_id, $new_meta );
-
-print_r( $new_meta );
-
-die;
+update_meta( $post_id, $new_meta );
