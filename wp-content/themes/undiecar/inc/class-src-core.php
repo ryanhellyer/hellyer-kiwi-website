1df9be3c2e6172f71aabbe69fee082add6f7e9cc
Ryan Hellyer <ryanhellyer@gmail.com>
2019-11-23 14:01:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/inc/class-src-core.php
+++ wp-content/themes/undiecar/inc/class-src-core.php
@@ -1031,10 +1031,12 @@ if ( isset( $_GET['test'] ) ) {
 			}
 
 		}
-
+echo $name . "\n";
+if ( $name === 'Hugo Olsson' ) {
+print_r( $result );die;
+}
 		// Add bonus point for leading race.
 		if ( 0 != $result['laps_led'] ) {
-print_r( $result );die;
 		}
 		if ( is_numeric( $result['laps_led'] ) && 0 < $result['laps_led'] ) {
 			$stored_results[$name] = $stored_results[$name] + 2;
