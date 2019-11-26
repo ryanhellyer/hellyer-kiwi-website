f49db705bd8adb99af3155b2759678494c2f6d35
Ryan Hellyer <ryanhellyer@gmail.com>
2019-11-26 11:01:09 +0000

    Automated git commit

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1c263c428b5a36b29acfa1b7347f103a::getLoader();
+return ComposerAutoloaderInit65d5ef00b1eea6a7cd7eecf821be7f46::getLoader();
