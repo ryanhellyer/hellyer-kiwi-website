660467cabb210e67b195711a5a8ea3f1ded3f298
Ryan Hellyer <ryanhellyer@gmail.com>
2019-11-26 22:22:01 +0000

    Automated git commit

--- wp-content/themes/undiecar/tools/modify-results.php
+++ wp-content/themes/undiecar/tools/modify-results.php
@@ -4,19 +4,18 @@ if ( ! isset( $_GET['modify_result'] ) ) {
 	return;
 }
 
-$post_id = 4033;
+$post_id = 4035;
 
-$meta = get_post_meta( $post_id, '_results_1', true );
+$meta = get_post_meta( $post_id, '_results_2', true );
 $new_meta = $old_meta = json_decode( $meta, true );
 
 //print_r( $new_meta[0] );die;
 
-$new_meta[0]['position'] = 1;
-$new_meta[1]['position'] = 2;
 $new_meta[2]['position'] = 3;
 $new_meta[3]['position'] = 4;
 $new_meta[4]['position'] = 5;
 $new_meta[5]['position'] = 6;
+$new_meta[6]['position'] = 2;
 
 
 /*
