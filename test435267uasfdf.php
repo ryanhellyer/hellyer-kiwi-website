a093813b0e2729f4b06ad59564bcf3ed5c63b8ef
Ryan Hellyer <ryanhellyer@gmail.com>
2020-02-28 02:50:01 +0000

    Automated git commit

--- test435267uasfdf.php
+++ test435267uasfdf.php
@@ -1,45 +1,42 @@
 <?php
+set_time_limit( 60 * 5 );
 
+$iterations_to_do = '';
+if ( isset( $_POST['iterations'] ) ) {
+	$iterations_to_do = (int) $_POST['iterations'];
+}
 
-echo '
+$form = '
 <form action="" method="POST">
-	<label>Iterations</label>
-	<input type="number" name="iterations" />
+	<label>Iterations (maxes out around 32 ish)</label>
+	<input type="number" name="iterations" value="' . $iterations_to_do . '" />
 
 	<br /><br />
 	<input type="submit" name="submit" value="submit" />
 </form>';
 
-if ( isset( $_POST ) ) {
-	print_r( $_POST );
+if ( ! isset( $_POST['iterations'] ) ) {
+	echo $form;
+	die;
 }
 
-die;
-
-$_GET['iterations'] = (int) $_GET['iterations'];
-
-set_time_limit( 60 * 5 );
-
 $args = array(
-	1  => 'Slowest 1 ',
-	2  => 'Test 2    ',
-	3  => 'Test 3    ',
-	4  => 'Test 4    ',
-	5  => 'Test 5    ',
-	6  => 'Fastest 6 ',
-//	7  => 'Test 7    ',
-//	8  => 'Test 8    ',
-//	9  => 'Test 9    ',
-//	10 => 'Test 10    ',
-	7 => 'Plugin on ',
-	8 => 'Plugin off',
+	1  => 'Plugin off',
+	2  => 'Slowest 1 ',
+	3  => 'Test 2    ',
+	4  => 'Test 3    ',
+	5  => 'Test 4    ',
+	6  => 'Test 5    ',
+	7  => 'Fastest 6 ',
+	8  => 'Plugin on ',
 );
 
 $url = 'ab -A zsuraski:M1qbewV6D9 -n 100 -c 20 https://zsuraski.site.strattic.io/?test=';
-echo '<p>' . $url . '</p>';
+echo '<pre>' . $url . '</pre><br />';
 
 $iterations = 0;
-while ( $iterations < $_GET['iterations'] ) {
+$iterations_to_do = $iterations_to_do * count( $args );
+while ( $iterations < $iterations_to_do ) {
 
 	$count++;
 
@@ -56,6 +53,7 @@ while ( $iterations < $_GET['iterations'] ) {
 	$iterations++;
 }
 
+echo '<pre>';
 foreach ( $args as $count => $label ) {
 	$number  = count( $results[ $count ] );
 	$average = array_sum( $results[ $count ] ) / $number;
@@ -64,3 +62,7 @@ foreach ( $args as $count => $label ) {
 
 	echo $label . ' average: ' . round( $average, 2 ) . ' (from: ' . $number . ' tests; max: ' . $max . '; min: ' . $min . ') ' . '<br />';
 }
+echo '</pre>';
+
+echo '<br /><br />';
+echo $form;
