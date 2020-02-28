c0bf5dbe30297d1fb245cc78696baf50816e1bfd
Ryan Hellyer <ryanhellyer@gmail.com>
2020-02-28 01:40:02 +0000

    Automated git commit

--- test435267uasfdf.php
+++ test435267uasfdf.php
@@ -1,11 +1,11 @@
 <?php
 
 $iterations = 0;
-while ( $iterations < 6 ) {
+while ( $iterations < 30 ) {
 
 	$count++;
 
-	$requests = shell_exec( 'ab -A zsuraski:M1qbewV6D9 -n 100 -c 20 https://zsuraski.site.strattic.io/?test=' . $count . ' | grep Request' );
+	$requests = shell_exec( 'ab -A zsuraski:M1qbewV6D9 -n 10 -c 5 https://zsuraski.site.strattic.io/?test=' . $count . ' | grep Request' );
 	$requests = str_replace( 'Requests per second:    ', '', $requests );
 	$requests = str_replace( ' [#/sec] (mean)', '', $requests );
 
@@ -19,14 +19,14 @@ while ( $iterations < 6 ) {
 }
 
 foreach ( array(
-	1 => 'Test 1',
-	2 => 'Plugin on',
+	1 => 'Test 1    ',
+	2 => 'Plugin on ',
 	3 => 'Plugin off'
 ) as $count => $label ) {
-	$count   = count( $results[ $count ] );
-	$average = array_sum( $results[ $count ] ) / $count;
-	$max     = max( $results[ $count ] );
-	$min     = min( $results[ $count ] );
+	$number  = count( $results[ $count ] );
+	$average = array_sum( $results[ $count ] ) / $number;
+	$max     = trim( max( $results[ $count ] ) );
+	$min     = trim( min( $results[ $count ] ) );
 
-	echo $label . ' average: ' . $average . ' (from: _' . $count . '_ tests; max: ' . $max . '; min: ' . $min . ') ' . "\n";
+	echo $label . ' average: ' . $average . ' (from: _' . $count . '_ tests; max: _' . $max . '_; min: _' . $min . '_) ' . "\n";
 }
